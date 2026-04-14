cbb1287f47229919537214d8bcb95a5efbfc30fc
ahmed saleban <ahmedsalebaancrtan@gmail.com>
2026-04-14 03:28:35 -0700

    completed setup project

---
+++ Backend/models/user.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        uint      `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"Createdat"`
+	UpdatedAt time.Time `json:"UpdatedAt"`
+}
