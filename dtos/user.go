238838cff4708964edf32d4f65b3ade59ac8ca11
ahmed saleban <ahmedsalebaancrtan@gmail.com>
2026-04-16 03:21:19 -0700

    added login user api

--- dtos/user.go
+++ dtos/user.go
@@ -1,7 +1,20 @@
 package dtos
 
+import "github.com/ahmedsaleban/eventManagementsystem/models"
+
 type CreateUserdto struct {
 	Name     string `json:"name" binding:"required"`
 	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
+	Password string `json:"password" binding:"required,min=8,max=128"`
+}
+
+type CreateLogindto struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8,max=128"`
+}
+
+type LoginUserResponse struct {
+	AccessToken  string      `json:"Access_token"`
+	RefreshToken string      `json:"Refresh_token"`
+	User         models.User `json:"user"`
 }
---
+++ helpers/jwt.go
@@ -0,0 +1,31 @@
+package helpers
+
+import (
+	"time"
+
+	"github.com/ahmedsaleban/eventManagementsystem/infra"
+	"github.com/ahmedsaleban/eventManagementsystem/models"
+	"github.com/golang-jwt/jwt"
+)
+
+func GenerateJwt(role models.Role, sub string, ExpireIn int64, isrefreshToken bool) (string, error) {
+	config := infra.Configuration
+
+	var jwtsecret []byte
+
+	if isrefreshToken {
+		jwtsecret = []byte(config.Refresh_jwt_token)
+	} else {
+		jwtsecret = []byte(config.Access_jwt_Token)
+	}
+	claims := jwt.MapClaims{
+		"sub":            sub,
+		"npf":            time.Now(),
+		"exp":            ExpireIn,
+		"isrefreshToken": isrefreshToken,
+		"role":           role,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtsecret)
+
+}
